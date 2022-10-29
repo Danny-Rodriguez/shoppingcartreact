5116ab60c2cc040523215ac74fb3b69afb47355f
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2022-10-28 17:05:21 -0700

    added modal

--- src/pages/Store.js
+++
@@ -1,5 +0,0 @@
-function Store() {
-  return <h1>Welcome to the Store</h1>
-}
-
-export default Store
---
+++ src/pages/Store.jsx
@@ -0,0 +1,23 @@
+import { Row, Col } from "react-bootstrap"
+import { productsArray } from "../ProductsStore"
+import ProductCard from "../components/ProductCard"
+
+function Store() {
+  return (
+    <>
+      <h1 align="center" className="p-3">
+        Welcome to the Store
+      </h1>
+      <Row xs={1} md={3} className="g-4">
+        {productsArray.map((product, index) => (
+          <Col align="center" key={index}>
+            <h1>{product.title}</h1>
+            <ProductCard product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default Store
